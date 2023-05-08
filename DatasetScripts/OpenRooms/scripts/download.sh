93c4c7a992c8d27ec9dc4330ccb82026e3dd04b1
JundanLuo <jundanluo22@gmail.com>
2023-05-08 13:10:46 +0100

    add install.sh and download.sh

---
+++ DatasetScripts/OpenRooms/scripts/download.sh
@@ -0,0 +1,28 @@
+# Image
+
+# Material
+
+# Geometry
+gdown https://drive.google.com/uc?id=\
+1FRPMKIDzFN1puk27MMGaIxCoi6HmLGyE
+gdrive files download 1FRPMKIDzFN1puk27MMGaIxCoi6HmLGyE
+#gdrive files download 11HNfjg50Ku5ufEwiLzvRJ_SVzsk0eYU2
+
+# LightSource
+gdown https://drive.google.com/uc?id=\
+1L-oUvZ9bD89fcqUyl14qgIQaCZHE_obo
+
+# Mask
+
+# Shading
+gdown https://drive.google.com/uc?id=\
+1lZsGEyeeUbUl-i68nIMuuT85Sptc6XPi
+
+# ShadingDirect
+gdown https://drive.google.com/uc?id=\
+1KMbo5e5lAPLztEiM301dPBPFQ97mWhTA
+
+# SVSG
+gdown https://drive.google.com/uc?id=\
+1MltE_Hoyb1jNV4p3H6n7NogCWQ9cFFQ_
+gdrive files download 1MltE_Hoyb1jNV4p3H6n7NogCWQ9cFFQ_
---
+++ DatasetScripts/OpenRooms/scripts/install.sh
@@ -0,0 +1,6 @@
+conda create -n data_process python=3.8
+pip install gdown
+conda install scipy scikit-image numpy matplotlib
+pip install opencv-python
+pip install kornia
+pip install kornia[x]
