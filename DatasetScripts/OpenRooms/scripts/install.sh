d3b49f21e836269d15f696d42589d3b540f46ed3
JundanLuo <jundanluo22@gmail.com>
2023-05-09 16:24:29 +0100

    modify install.sh

--- DatasetScripts/OpenRooms/scripts/install.sh
+++ DatasetScripts/OpenRooms/scripts/install.sh
@@ -1,6 +1,7 @@
 conda create -n data_process python=3.8
 pip install gdown
-conda install scipy scikit-image numpy matplotlib
 pip install opencv-python
+pip install matplotlib
+conda install scipy scikit-image numpy
 pip install kornia
 pip install kornia[x]
